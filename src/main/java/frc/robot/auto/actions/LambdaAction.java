ae01c96930f7e9e57a088de5be5b16916d90622c
Mustafa Miyaziwala <mustafa_miyazi@hotmail.com>
2020-01-09 19:46:55 -0800

    Add basic and drive specific actions

---
+++ src/main/java/frc/robot/auto/actions/LambdaAction.java
@@ -0,0 +1,40 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.auto.actions;
+
+/**
+ * Add your docs here.
+ */
+public class LambdaAction implements Action {
+
+    public interface VoidSupplier {
+        void f();
+    }
+
+    VoidSupplier mF;
+
+    public LambdaAction(VoidSupplier f) {
+        this.mF = f;
+    }
+
+    @Override
+    public void start() {
+        mF.f();
+    }
+
+    @Override
+    public void update() {}
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+
+    @Override
+    public void done() {}
+}
