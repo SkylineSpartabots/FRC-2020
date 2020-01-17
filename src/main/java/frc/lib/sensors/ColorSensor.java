8d5da1fc5ded74c490a100d4823a1a56ba40d255
Vibhav Peri <vibhavperi@gmail.com>
2020-01-16 20:20:43 -0800

    Vibhav color sensor magic

--- src/main/java/frc/lib/sensors/ColorSensor.java
+++ src/main/java/frc/lib/sensors/ColorSensor.java
@@ -19,10 +19,11 @@ import edu.wpi.first.wpilibj.util.Color;
  */
 public class ColorSensor {
 
-    private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
-    private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
-    private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.144);
-    private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
+    private final Color kBlueTarget = ColorMatch.makeColor(0.109375, 0.427978515625, 0.462646484375);
+    private final Color kGreenTarget = ColorMatch.makeColor(0.149169921875 , 0.597412109375 , 0.253662109375);
+    private final Color kRedTarget = ColorMatch.makeColor(0.5498046875, 0.330810546875, 0.119384765625);
+    private final Color kYellowTarget = ColorMatch.makeColor(0.31787109375, 0.570556640625, 0.111572265625);
+    private final Color kWhiteTarget = ColorMatch.makeColor(0.2861328125, 0.48291015625, 0.23095703125);
 
     private final ColorSensorV3 mColorSensor;
     private final ColorMatch mColorMatcher;
@@ -39,14 +40,14 @@ public class ColorSensor {
         mColorMatcher.addColorMatch(kGreenTarget);
         mColorMatcher.addColorMatch(kRedTarget);
         mColorMatcher.addColorMatch(kYellowTarget);
-
+        mColorMatcher.addColorMatch(kWhiteTarget);
         
     }
 
     public Colors getColor() {
         Color detectedColor = mColorSensor.getColor();
         ColorMatchResult match = mColorMatcher.matchClosestColor(detectedColor);
-
+    
         if(match.color == kBlueTarget) {
             return Colors.BLUE;
         } else if (match.color == kRedTarget) {
